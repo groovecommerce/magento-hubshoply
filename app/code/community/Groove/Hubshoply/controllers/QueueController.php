54ba9e8553a18203e567ce3ede58e2c14d17a2cf
rbuczynski <rick@groovecommerce.com>
2017-06-20 21:48:38 -0400

    Queue controller cleanup, optimizations, add logging

--- app/code/community/Groove/Hubshoply/controllers/QueueController.php
+++ app/code/community/Groove/Hubshoply/controllers/QueueController.php
@@ -47,6 +47,97 @@ class Groove_Hubshoply_QueueController
 {
 
     /**
+     * Check for authorization. Halt with a 401 if unauthorized, else return token.
+     * 
+     * @return Groove_Hubshoply_Model_Token
+     */
+    private function _checkAuthorization()
+    {
+        $accessToken    = $this->getRequest()->getHeader('X-Access-Token');
+        $tokenModel     = Mage::getModel('groove_hubshoply/token')->load($accessToken, 'token');
+
+        if ( !$tokenModel->getId() || $tokenModel->isExpired() ) {
+            $this->_sendError(
+                401,
+                'Unauthorized',
+                'Your token is invalid or not present. Please re-authenticate and try again.',
+                function ($response) { $response->setHeader('WWW-Authenticate', 'Custom', true); }
+            );
+        } else {
+            return $tokenModel;
+        }
+    }
+
+    /**
+     * Trigger abandaned cart queue processing.
+     *
+     * @return void
+     */
+    private function _queueCarts()
+    {
+        Mage::getModel('groove_hubshoply/event')->abandonCartProcessing(new Varien_Event_Observer());
+    }
+
+    /**
+     * Removes all queue items inclusively for the ID range given.
+     * 
+     * @param int $from The starting ID range value.
+     * @param int $to   The ending ID range value.
+     *
+     * @return void
+     */
+    private function _removeFromRange($from, $to)
+    {
+        try {
+            $this->_validateQueueIdRange($from, $to);
+
+            Mage::getResourceModel('groove_hubshoply/queueitem_collection')
+                ->addFieldToFilter('queue_id', array('from' => $from, 'to' => $to))
+                ->walk('delete');
+        } catch (Exception $error) {
+            Mage::helper('groove_hubshoply/debug')->logException($error);
+
+            $this->_sendError(500, 'Failed to remove queue items from range.', $error);
+        }
+    }
+
+    /**
+     * Terminates request with given error details.
+     *
+     * @param integer  $code       An HTTP response code.
+     * @param string   $message    An optional error message to include.
+     * @param string   $details    Optional details about the error.
+     * @param callable $callback   Optional callback to apply to the response.
+     *
+     * @return void
+     */
+    private function _sendError($code = null, $message = '', $details = '', $callback = null)
+    {
+        if ( (int) $code < 100 ) {
+            $code = 500;
+        }
+
+        $data = array(
+            'error_code'    => $code,
+            'error_message' => $message,
+            'error_details' => $details,
+        );
+
+        $this->getResponse()
+            ->setHttpResponseCode($code);
+            ->setBody(Mage::helper('core')->jsonEncode($data));
+
+        if (is_callable($callback)) {
+            $callback($this->getResponse());
+        }
+
+        $this->getResponse()
+            ->sendResponse();
+
+        exit;
+    }
+
+    /**
      * Validate the OAuth consumer data.
      * @param Mage_Oauth_Model_Consumer $consumer The consumer model.
      * 
@@ -55,14 +146,33 @@ class Groove_Hubshoply_QueueController
     private function _validateConsumer(Mage_Oauth_Model_Consumer $consumer)
     {
         if (!$consumer->getId()) {
-            throw new Mage_Oauth_Exception(401, 'Unrecognized OAuth client.');
+            throw new Mage_Oauth_Exception('Unrecognized OAuth client.', 401);
         }
 
         $helper = Mage::helper('groove_hubshoply');
         $secret = $this->getRequest()->getHeader('X-Auth-Secret');
 
         if ( !$helper->safeCompare($consumer->getSecret(), $secret) ) {
-            throw new Mage_Oauth_Exception(401, 'OAuth secret key rejected.');
+            throw new Mage_Oauth_Exception('OAuth secret key rejected.', 401);
+        }
+    }
+
+    /**
+     * Validate the queue ID range.
+     * 
+     * @param int $from The starting ID range value.
+     * @param int $to   The ending ID range value.
+     *
+     * @return void
+     */
+    private function _validateQueueIdRange($from, $to)
+    {
+        if ( !( is_numeric($from) && is_numeric($to) && ($from <= $to) ) ) {
+            $message = ($from === $to) ?
+                "Queue ID provided [$from] is an invalid integer." :
+                "Queue ID range provided [FROM $from, TO $to] is an invalid range of integers.";
+
+            $this->_sendError(400, 'Bad Request', $message);
         }
     }
 
@@ -99,6 +209,8 @@ class Groove_Hubshoply_QueueController
                 ->setBody(Mage::helper('core')->jsonEncode($response))
                 ->sendResponse();
         } catch (Mage_Oauth_Exception $error) {
+            Mage::helper('groove_hubshoply/debug')->logError($error);
+
             $this->_sendError($error->getCode(), 'OAuth Exception', $error->getMessage());
         } catch (Exception $error) {
             Mage::helper('groove_hubshoply/debug')->logError($error);
@@ -109,6 +221,8 @@ class Groove_Hubshoply_QueueController
 
     /**
      * View action.
+     *
+     * A customizable view of queue items.
      * 
      * Route: hubshoply/queue/view/first/:count
      * Route: hubshoply/queue/view/last/:count
@@ -123,8 +237,6 @@ class Groove_Hubshoply_QueueController
      * - Example:
      *  
      *  hubshoply/queue/view/first/:count/store/:store_id/type/:type
-     *
-     * A customizable view of queue items.
      *
      * @return void
      */
@@ -172,7 +284,7 @@ class Groove_Hubshoply_QueueController
         } catch (Exception $error) {
             Mage::helper('groove_hubshoply/debug')->logException($error);
 
-            $this->_errorBody(500, 'Failed to process view request', $error->getMessage(), null);
+            $this->_sendError(500, 'Failed to process view request.', $error->getMessage());
         }
     }
 
@@ -182,230 +294,43 @@ class Groove_Hubshoply_QueueController
      * Route: hubshoply/queue/mark/id/:id
      * Route: hubshoply/queue/mark/from/:id/to/:id
      *
-     * After processing queue items, send Queue_id (or a range) back
-     * This will delete the items from the queue
+     * Delete the requested items from the queue.
      *
      * @return void
      */
     public function markAction()
     {
-        $req = $this->getRequest();
+        try {
+            $request = $this->getRequest();
 
-        $this->_checkAuthorization();
+            $this->_checkAuthorization();
 
-        if(!$this->_isMethod('DELETE'))
-        {
-            //Returns 405 Method Not Allowed if it is not a DELETE method
-            //Sets Allow header to user indicating DELETE is the only allowed method
-            $this->_errorBody(405, 'Method Not Allowed',
-                'You have attempted to access this URI using the ['.$req->getMethod().'] method when DELETE is the only allowed method.',
-                function($response) { $response->setHeader('Allow','DELETE',true); }
-            );
-        }
-        elseif($id = $req->getParam('id'))
-        {
-            $this->_removeFromRange($id,$id);
-        }
-        elseif(
-            ($from = $req->getParam('from'))
-            && ($to = $req->getParam('to')))
-        {
-            $this->_removeFromRange($from,$to);
-        }
-        else
-        {
-            //return a 400 error if there is a bad/malformed URL request
-            $this->_errorBody(400, 'Bad Request',
-                'URI needs to be in the format [hubshoply/mark/id/###] for single deletions '
-                .'or [hubshoply/mark/from/###/to/###]. Your URI was ['.$req->getRequestUri().']'
-            );
-        }
-    }
+            if (!$request->isDelete()) {
+                $this->_sendError(
+                    405,
+                    'Method Not Allowed',
+                    'Requested method is not allowed.',
+                    function($response) { $response->setHeader('Allow', 'DELETE', true); }
+                );
+            } elseif ( ( $id = $request->getParam('id') ) ) {
+                $this->_removeFromRange($id,$id);
+            } elseif (
+                ( $from = $request->getParam('from') ) &&
+                ( $to = $request->getParam('to'))
+            ) {
+                $this->_removeFromRange($from, $to);
+            } else {
+                $this->_sendError(
+                    400,
+                    'Bad Request',
+                    'URI needs to be in the format [hubshoply/mark/id/###] for single deletions or [hubshoply/mark/from/###/to/###].'
+                );
+            }
+        } catch (Exception $error) {
+            Mage::helper('groove_hubshoply/debug')->logException($error);
 
-    /**
-     * Check for authorization. Halt with a 401 if unauthorized, else return token.
-     * 
-     * @return Groove_Hubshoply_Model_Token
-     */
-    private function _checkAuthorization()
-    {
-        //get Token from header
-        $token = $this->getRequest()->getHeader('X-Access-Token');
-        //attempt to load the token
-        $loaded = Mage::getModel('groove_hubshoply/token')->load($token,'token');
-        //test if token doesn't exist or isExpired
-        if(!$loaded->getId() || $loaded->isExpired())
-        {
-            //generate message based on if using a single ID or actual range.
-            $message = 'Your token is invalid or not present. Please re-authenticate and try again.';
-            $callback = function($response){
-                $response->setHeader('WWW-Authenticate','Custom',true);
-            };
-            //send error body and finish request
-            $this->_errorBody(401, 'Unauthorised',$message,$callback);
+            $this->_sendError(500, 'Failed to process mark request.', $error->getMessage());
         }
-        else
-        {
-            return $loaded;
-        }
-    }
-    /**
-     * Validate the requested method.
-     * 
-     * @param string $method Method to check for
-     *
-     * @return bool Returns TRUE if Request method is the same as the $method argument
-     */
-    private function _isMethod($method)
-    {
-        return strcasecmp($this->getRequest()->getMethod(), $method) == 0 ;
-    }
-
-    /**
-     * Creates a JSON Error message to throw, and sets appropriate response headers.
-     * Then sends response and terminates any further controller actions.
-     *
-     * @param int|string $code          HTTP Error Code
-     * @param string $message           HTTP Error Message
-     * @param string $details           Details about error
-     * @param null|callable $callback   Optional Callback to apply to response
-     *
-     * @throws Zend_Controller_Response_Exception
-     */
-    private function _errorBody($code,$message,$details, $callback = null)
-    {
-        //Response message
-        $response = array(
-            'error_code' => $code,
-            'error_message' => $message,
-            'error_details' => $details
-        );
-        //format as JSON
-        $response = json_encode($response);
-        //Set code and Response body
-        $this->getResponse()->setHttpResponseCode($code);
-        $this->getResponse()->setBody($response);
-        //Additional response actions if needed
-        if(is_callable($callback))
-        {
-            $callback($this->getResponse());
-        }
-        //Send response and terminate controller actions
-        $this->getResponse()->sendResponse();
-        exit;
-    }
-
-    /**
-     * Creates a JSON Error message to throw, and sets appropriate response headers.
-     * Then sends response and terminates any further controller actions.
-     *
-     * @param int|string $code          HTTP Error Code
-     * @param string $message           HTTP Error Message
-     * @param string $details           Details about error
-     * @param null|callable $callback   Optional Callback to apply to response
-     *
-     * @return void
-     */
-    private function _sendError($code,$message,$details, $callback = null)
-    {
-        //Response message
-        $response = array(
-            'error_code' => $code,
-            'error_message' => $message,
-            'error_details' => $details
-        );
-        //format as JSON
-        $response = json_encode($response);
-        //Set code and Response body
-        $this->getResponse()->setHttpResponseCode($code);
-        $this->getResponse()->setBody($response);
-        //Additional response actions if needed
-        if(is_callable($callback))
-        {
-            $callback($this->getResponse());
-        }
-        //Send response and terminate controller actions
-        $this->getResponse()->sendResponse();
-        exit;
-    }
-
-    /**
-     * Takes an exception and logs it,
-     * as well as terminates controller with a 500 error message
-     *  
-     * @param Exception $x
-     */
-    private function _handleException(Exception $x)
-    {
-        Mage::log(
-            $x->getMessage().PHP_EOL.$x->getTraceAsString()
-            ,null, $this::QUEUE_CONTROL_LOG
-        );
-        $this->_errorBody(500, 'Server Error',
-            "There was an error processing your request, please check your servers' log files or try again at a later date."
-        );
-    }
-
-    /**
-     * Validate the queue ID range.
-     * 
-     * @param int $from
-     * @param int $to
-     *
-     * @return bool Returns true if the range is valid numbers
-     */
-    private function _validateQueueIdRange($from,$to)
-    {
-        //check if range is valid
-        $valid = is_numeric($from)
-                 && is_numeric($to)
-                 && ($from <= $to);
-        //if invalid, return/throw error
-        if(!$valid)
-        {
-            //generate message based on if using a single ID or actual range.
-            $message = ($from === $to)
-                ?"Queue ID provided [$from] is an invalid integer."
-                :"Queue ID range provided [FROM $from, TO $to] is an invalid range of integers.";
-            //send error body and finish request
-            $this->_errorBody(400, 'Bad Request',$message);
-        }
-    }
-
-    /**
-     * Removes all queue items with ID's in the range ($from,$to) inclusive
-     * 
-     * @param int $from
-     * @param int $to
-     *
-     * @return void
-     */
-    private function _removeFromRange($from,$to)
-    {
-        //validate range. If invalid, it terminates before continuing further down.
-        $this->_validateQueueIdRange($from,$to);
-        try
-        {
-            //delete item if present. If item doesn't exist, it is pretty much already deleted
-            Mage::getModel('groove_hubshoply/queueitem')
-                ->getCollection()
-                ->addFieldToFilter('queue_id', array('from'=>$from,'to'=>$to))
-                ->walk('delete');
-        }
-        catch(Exception $x)
-        {
-            $this->_handleException($x);
-        }
-    }
-
-    /**
-     * Calls our helper that queues up abandoned carts
-     *
-     * @return void
-     */
-    private function _queueCarts()
-    {
-        Mage::getModel('groove_hubshoply/event')->abandonCartProcessing(new Varien_Event_Observer());
     }
     
 }
